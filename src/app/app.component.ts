cab708cb1d8d68be1b605b57d693fcd290c6737d
Matheus Costa <matheuscostamonteiro.dev@gmail.com>
2019-05-05 00:12:36 -0300

    latest commit before lauching the analyst version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,18 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { TabsPage } from '../pages/tabs/tabs';
 import { IntroPage } from '../pages/intro/intro';
 
-
-
 @Component({
-  templateUrl: 'app.html'
+	templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any  = localStorage.getItem('keyName') ? TabsPage : IntroPage;
+	rootPage: any = localStorage.getItem('keyName') ? TabsPage : IntroPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+		platform.ready().then(() => {
+			// Okay, so the platform is ready and our plugins are available.
+			// Here you can do any higher level native things you might need.
+			statusBar.styleDefault();
+			splashScreen.hide();
+		});
+	}
 }
--- src/pages/pressao/pressao.ts
+++ src/pages/pressao/pressao.ts
@@ -1,95 +1,77 @@
-import { Component , ViewChild} from '@angular/core';
-import { IonicPage, NavController, NavParams,ToastController } from 'ionic-angular';
-
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-pressao',
-  templateUrl: 'pressao.html',
+	selector: 'page-pressao',
+	templateUrl: 'pressao.html'
 })
 export class PressaoPage {
- 	@ViewChild('pres1') pres1;
+	@ViewChild('pres1') pres1;
 	@ViewChild('pres2') pres2;
 	@ViewChild('nome') nome;
-	// normal: boolean;
-	// pre: boolean;
-	// resultado1 : string;
-	// resultado2 : string;
 	resultado: string;
-	name : string;
-	resultado_alto_baixo
-  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
-  }
+	name: string;
+	resultado_alto_baixo;
+	constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {}
 
+	pressao() {
+		var toast = this.toastCtrl.create({
+			duration: 6000,
+			showCloseButton: true,
+			closeButtonText: 'OK',
+			position: 'bottom'
+		});
+		var pres1 = this.pres1.value;
+		var pres2 = this.pres2.value;
 
-pressao(){
-	 var toast = this.toastCtrl.create(({duration: 6000,showCloseButton: true,closeButtonText: "OK", position :'bottom'}));
-	 var pres1 =  this.pres1.value;
-	 var pres2 = this.pres2.value;
-	//  this.resultado1 = pres1;
-	//  this.resultado2 = pres2;
-	 this.resultado = '';
-	 this.resultado_alto_baixo = ''
-	 this.name = this.nome.value;
+		this.resultado = '';
+		this.resultado_alto_baixo = '';
+		this.name = this.nome.value;
 
-// if (pres1 >= 140) {
-// 	pres1 = true;
-// }
-// if (pres2 >= 90) {
-// 	pres2 = true;
-// } 
+		if (pres1 < 90 || pres2 < 60) {
+			this.resultado = 'BAIXA';
+			this.resultado_alto_baixo = 'baixa';
+			toast.setMessage('Sua pressão arterial pode estar baixa' + ', ' + this.nome.value + ' procure um médico');
+			toast.present();
+		}
 
-if (pres1 < 90 || pres2 < 60){
-this.resultado = 'BAIXA';
-this.resultado_alto_baixo = 'baixa'
-toast.setMessage("Sua pressão arterial pode estar baixa" + ", " + this.nome.value + " procure um médico");
-toast.present();
-}
+		if ((pres1 >= 90 && pres1 <= 120) || (pres2 >= 60 && pres2 <= 80)) {
+			this.resultado = 'NORMAL';
+			this.resultado_alto_baixo = 'normal';
+			toast.setMessage('Sua pressão arterial está normal' + ', ' + this.nome.value);
+			toast.present();
+		}
 
-if ((pres1 >= 90 && pres1 <= 120) || (pres2>=60 && pres2 <= 80)) {
-this.resultado = 'NORMAL';
-this.resultado_alto_baixo = 'normal'
-toast.setMessage("Sua pressão arterial está normal" + ", " + this.nome.value);
-toast.present();
-}
+		if ((pres1 >= 121 && pres1 <= 139) || (pres2 >= 81 && pres2 <= 89)) {
+			this.resultado = 'PRÉ HIPERTENSÃO';
+			this.resultado_alto_baixo = 'um pouco elevada';
+			toast.setMessage('Cuidado! Você pode estar com pré-hipertensão' + ', ' + this.nome.value);
+			toast.present();
+		}
 
-if ( (pres1 >=121 && pres1<=139) || (pres2>=81 && pres2<=89) ) {
-this.resultado = 'PRÉ HIPERTENSÃO';
-this.resultado_alto_baixo = 'um pouco elevada';
-toast.setMessage("Cuidado! Você pode estar com pré-hipertensão" + ", " + this.nome.value);
-toast.present();
-}
+		if ((pres1 >= 140 && pres1 <= 159) || (pres2 >= 90 && pres2 <= 99)) {
+			this.resultado = 'HIPERTENSÃO ESTÁGIO 1';
+			this.resultado_alto_baixo = 'elevada';
+			toast.setMessage('Cuidado! Você pode estar com Hipertensão estágio 1' + ', ' + this.nome.value);
+			toast.present();
+		}
 
-if ((pres1 >=140 && pres1<=159) || (pres2>=90 && pres2<=99)) {
-this.resultado = 'HIPERTENSÃO ESTÁGIO 1';
-this.resultado_alto_baixo = 'elevada';
-toast.setMessage("Cuidado! Você pode estar com Hipertensão estágio 1" + ", " + this.nome.value);
-toast.present();
-}
+		if ((pres1 >= 160 && pres1 <= 179) || (pres2 >= 100 && pres2 <= 109)) {
+			this.resultado = 'HIPERTENSÃO ESTÁGIO 2';
+			this.resultado_alto_baixo = 'muito elevada';
+			toast.setMessage('Cuidado! Você pode estar com Hipertensão estágio 2 ' + ', ' + this.nome.value);
+			toast.present();
+		}
 
-if ((pres1 >=160 && pres1<=179) || (pres2>=100 && pres2<=109)) {
-this.resultado = 'HIPERTENSÃO ESTÁGIO 2';
-this.resultado_alto_baixo = 'muito elevada';
-toast.setMessage("Cuidado! Você pode estar com Hipertensão estágio 2 " + ", " + this.nome.value);
-toast.present();
-}
-
-if (pres1>=180 || pres2>=110) {
-this.resultado = 'HIPERTENSÃO ESTÁGIO 3';
-this.resultado_alto_baixo = 'muitíssimo elevada';
-toast.setMessage("Cuidado! Você pode estar com Hipertensão estágio 3" + ", " + this.nome.value);
-toast.present();
-}
-
-
-// else if(pres1>=140 || pres2>=90){
-// 	toast.setMessage("Cuidado! Você pode estar com Hipertensão" + ", " + this.nome.value);
-// 	toast.present();
-// }
-
-}
-AlimentosPage(){
-	 this.navCtrl.push('NutrientesPage');
-}
-
+		if (pres1 >= 180 || pres2 >= 110) {
+			this.resultado = 'HIPERTENSÃO ESTÁGIO 3';
+			this.resultado_alto_baixo = 'muitíssimo elevada';
+			toast.setMessage('Cuidado! Você pode estar com Hipertensão estágio 3' + ', ' + this.nome.value);
+			toast.present();
+		}
+	}
+	AlimentosPage() {
+		this.navCtrl.push('NutrientesPage');
+	}
 }
